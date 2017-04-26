43608c64a8b3607385d60727b174ce54f5770706
ebordeau <ebordeau@gmail.com>
2017-04-25 22:24:37 -0400

    Initial Atom commit

---
+++ spec/lightning-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LightningEditorView from '../lib/lightning-editor-view';
+
+describe('LightningEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
